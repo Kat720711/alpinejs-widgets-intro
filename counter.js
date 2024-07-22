f5ca9d7932e6e37ec69f59ccb7b169e90bec6f43
Kat720711 <ckmakofane2001@gmail.com>
2024-07-22 10:10:33 +0200

    gh-pages

--- counter.js
+++ counter.js
@@ -1,15 +1,27 @@
-let counter = 0;
+function Counter() {
+    return {
+        count: 0,
+        maxCount: 10, 
+        minCount: 0,  
 
-const counterElem = document.querySelector('.counter');
-const incrementBtn =  document.querySelector('.increment');
-const decrementBtn = document.querySelector('.decrement');
+        increment() {
+            if (this.count < this.maxCount) {
+                this.count++;
+            }
+        },
 
-incrementBtn.addEventListener('click', function() {
-    counter++;
-    counterElem.innerText = counter;
-});
+        decrement() {
+            if (this.count > this.minCount) {
+                this.count--;
+            }
+        },
 
-decrementBtn.addEventListener('click', function() {
-    counter--;
-    counterElem.innerText = counter;
+        reset() {
+            this.count = 0;
+        }
+    }
+}
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('counter', Counter);
 });
